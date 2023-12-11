8c5a16b673c65808c94da76f9c49cdf401ba119c
Utibeabasi Umanah <utibeabasiumanah6@gmail.com>
2023-12-11 09:40:40 +0100

    feat: initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions, Grid, Container } from '@mui/material';
+
 
 function App() {
+  const [data, setData] = useState()
+  useEffect(() => {
+    async function fetchData() {
+      // You can await here
+      const response = await fetch("https://big-kitchen-petite.functions.dev.on-fleek.app/invoke?query=iphone");
+      return response;
+    }
+    fetchData().then(res => res.json()).then(r => setData(r))
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Container fluid>
+    <Typography margin={10} variant="h3" align="center">Welcome to my news site</Typography>
+    <Container fluid>
+      <Grid container spacing={2}>
+        {data && data.news.articles.map((dataObj, index) => {
+          return (
+            <Grid item><Card sx={{ maxWidth: 345 }}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image={dataObj.urlToImage}
+                  alt="news"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {dataObj.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {dataObj.description}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button href={dataObj.url} size="small" color="primary">
+                  VIEW
+                </Button>
+              </CardActions>
+            </Card></Grid>
+          );
+        })}</Grid></Container></Container></>
   );
 }
 
